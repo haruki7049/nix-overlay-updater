b6e68174f1bc22f427c0fe3c3db14fde10c8b6a1
haruki7049 <tontonkirikiri@gmail.com>
2025-05-31 23:10:09 +0900

    Update

---
+++ README.md
@@ -0,0 +1,1 @@
+# nix-overlay-updater
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@ use clap::Parser;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: CLIArgs = CLIArgs::parse();
 
-    let url: String = format!("https://api.github.com/repos/{}/{}/releases", args.owner, args.repo);
+    let url: String = format!(
+        "https://api.github.com/repos/{}/{}/releases",
+        args.owner, args.repo
+    );
     let client = reqwest::Client::new();
     let response = client.get(url);
     println!("{response:#?}");

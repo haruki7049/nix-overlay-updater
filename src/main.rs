3edaad95918a54a3e9003156a36228d95e40c1bd
haruki7049 <tontonkirikiri@gmail.com>
2025-05-31 23:05:27 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,13 @@
-fn main() {
+use std::collections::HashMap;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let owner: &str = "denoland";
+    let repo: &str = "deno";
+    let url: String = format!("https://api.github.com/repos/{}/{}/releases", owner, repo);
+
+    let client = reqwest::Client::new();
+    let response = client.get(url);
+    println!("{response:#?}");
+    Ok(())
 }

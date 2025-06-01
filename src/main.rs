5ec71eac46db8248499aaff448678c99fb2fed83
haruki7049 <tontonkirikiri@gmail.com>
2025-06-01 10:04:30 +0900

    style: nix fmt

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
+use serde::{Deserialize, Serialize};
 use url::Url;
-use serde::{Serialize, Deserialize};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
